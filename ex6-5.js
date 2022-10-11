d690bc03bbe0cec97e6634286098bae786b5a46f
BellaBall <114452811+BellaBall@users.noreply.github.com>
2022-10-11 10:14:11 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,10 @@
-var who65'world';
-function sayhello65(){
-  alert('hello
+var BellaBall = 'world';
+function sayhello65() {
+  alert('Hello, ' + who65 + '!');
+}
+function taro65() {
+  BellaBall = '太郎';
+}
+function hanako65() {
+  BellaBall = '花子';
+}
