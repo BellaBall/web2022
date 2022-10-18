a9f00f2204a54e1c4e1d278ade183349b5fec8e8
BellaBall <114452811+BellaBall@users.noreply.github.com>
2022-10-18 10:21:43 +0900

    Update ex06-7.js

--- ex06-7.js
+++ ex06-7.js
@@ -2,10 +2,10 @@ var who67 = 'world';
 function sayhello67() {
 alert('Hello, ' + who67 + '!');
 }
-function someone(太郎，花子) {
+function someone(太郎) {
 who67 = 太郎;
 }
 
-function someone(太郎，花子) {
+function someone(花子) {
 who67 = 花子;
 }
