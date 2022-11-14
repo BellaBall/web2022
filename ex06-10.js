9585b727579126cf7139bd0687f162cb4b90e62f
BellaBall <114452811+BellaBall@users.noreply.github.com>
2022-11-14 19:00:36 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){ document.getElementById('logo').src='./image/josairogo.png';}
-function imgchange2(){ document.getElementById('logo').src='./image/josairogoname.jpg';}
+function imgchange1(){ document.getElementById('logo').src='./image/image.png';}
+function imgchange2(){ document.getElementById('logo').src='./image/Josai_University_Logo.png';}
