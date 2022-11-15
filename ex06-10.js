5a90fa1be94f72417c0aa25b1699084f4ff5fb8f
BellaBall <114452811+BellaBall@users.noreply.github.com>
2022-11-15 10:09:30 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){ document.getElementById('logo').src='./image/JOSAI 1';}
-function imgchange2(){ document.getElementById('logo').src='./image/JOSAI 2';}
+function imgchange1(){ document.getElementById('logo').src='./image/JOSAI 1.png';}
+function imgchange2(){ document.getElementById('logo').src='./image/JOSAI 2.png';}
