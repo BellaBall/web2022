105fa1f560df33f939d1fe11add66d79118cc39b
BellaBall <114452811+BellaBall@users.noreply.github.com>
2022-11-15 10:09:02 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688() {document.getElementById('who688').innerHTML='<img src="./image/JOSAI 2" alt="城西ロゴ名前入り">';}
+function gazo688() {document.getElementById('who688').innerHTML='<img src="./image/JOSAI 2.png" alt="城西ロゴ名前入り">';}
